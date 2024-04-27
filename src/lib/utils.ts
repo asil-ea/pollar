0a631cf4cd50bb6d8ba9a9f017ce164ed2a0ffe9
Asil <asilefeakyol@gmail.com>
2024-04-27 22:36:25 +0300

    Extract util functions

---
+++ src/lib/utils.ts
@@ -0,0 +1,26 @@
+import { IFormResponse } from "./types";
+
+export const performCreateSurveyFormValidation = (
+  formResponse: IFormResponse
+) => {
+  const errors = [];
+  if (
+    parseInt(formResponse["questionCount"].toString()) < 0 ||
+    parseInt(formResponse["questionCount"].toString()) > 10
+  ) {
+    errors.push("You must provide between 1 and 10 survey questions.");
+  }
+
+  if (
+    parseInt(formResponse["optionCount"].toString()) < 2 ||
+    parseInt(formResponse["optionCount"].toString()) > 5
+  ) {
+    errors.push("You must provide between 2 and 5 survey options.");
+  }
+
+  if (errors.length > 0) {
+    return { error: errors.join(" - ") };
+  }
+
+  return null;
+};
