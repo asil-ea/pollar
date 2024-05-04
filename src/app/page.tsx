7f02d3cedcc4647fbae47bf81f5b6235c3715d31
Asil <asilefeakyol@gmail.com>
2024-05-04 16:03:49 +0300

    Implement Hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,25 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <div className="">
-        <br />
+      <div className="relative isolate px-6 pt-14 lg:px-8">
+        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+          <div className="text-center">
+            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+              Use the power of AI to Survey
+            </h1>
+            <p className="mt-6 text-lg leading-8 text-gray-600">
+              Create surveys and predict question results in seconds.
+            </p>
+            <div className="mt-6 flex items-center justify-center gap-x-6">
+              <Link
+                href="/login"
+                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Get started
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
