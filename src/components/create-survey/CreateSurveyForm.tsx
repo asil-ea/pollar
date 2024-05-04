1e3c7e1db56f73f6dcd5684b85548158fcb67bec
Asil <asilefeakyol@gmail.com>
2024-05-04 23:15:54 +0300

    Create Survey styling

--- src/components/create-survey/CreateSurveyForm.tsx
+++ src/components/create-survey/CreateSurveyForm.tsx
@@ -53,7 +53,6 @@ const CreateSurveyForm = ({}: {}) => {
                       type="text"
                       name="questionCount"
                       id="questionCount"
-                      autoComplete="given-name"
                       className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                       disabled={formStatus.pending}
                     />
@@ -71,7 +70,6 @@ const CreateSurveyForm = ({}: {}) => {
                       type="text"
                       name="optionCount"
                       id="optionCount"
-                      autoComplete="given-name"
                       className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                       disabled={formStatus.pending}
                     />
--- src/components/create-survey/CreateSurveyResult.tsx
+++ src/components/create-survey/CreateSurveyResult.tsx
@@ -3,6 +3,8 @@ import React, { useEffect, useMemo, useState } from "react";
 
 const CreateSurveyResult = ({ state }: { state: any }) => {
   const [isGenerated, setIsGenerated] = useState(false);
+  const [selectAll, setSelectAll] = useState(false);
+
   const possibleOptions = useMemo(() => {
     if (!state.options) {
       return [];
@@ -31,25 +33,33 @@ const CreateSurveyResult = ({ state }: { state: any }) => {
   }
 
   return (
-    <div className="container mx-2 my-6">
-      {/* title */}
-      <h1 className="text-base font-semibold leading-7 text-gray-900">
-        {state.surveyTitle}
-      </h1>
-      <ol>
+    <div className="container mx-auto my-6 p-4 bg-gray-100 rounded-md">
+      <div className="flex items-center mb-2">
+        <h1 className="text-2xl font-semibold leading-7 text-gray-900 mb-4">
+          {state.surveyTitle}
+        </h1>
+      </div>
+      <p>Select all the options you would like to add to the Survey.</p>
+      <ol className="list-decimal pl-6">
         {state.questions.map((question: any, index: number) => (
           <li key={index} className="mb-4">
-            <h2>
-              {index + 1}. {question.title}
-            </h2>
+            <div className="flex items-center">
+              <input type="checkbox" className="mr-2" />
+              <h2 className="text-lg font-medium">{question.title}</h2>
+            </div>
             <ul className="flex justify-between">
               {possibleOptions.map((option: string, index: number) => (
-                <li key={index}>{option}</li>
+                <li key={index} className="px-2 py-1 bg-gray-200 rounded-md">
+                  {option}
+                </li>
               ))}
             </ul>
           </li>
         ))}
       </ol>
+      <button className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:cursor-not-allowed disabled:bg-gray-300 disabled:text-gray-900 disabled:shadow-none disabled:hover:bg-gray-300 disabled:focus-visible:outline-gray-300 disabled:focus-visible:outline-offset-0 disabled:focus-visible:outline-none">
+        Add to Survey
+      </button>
     </div>
   );
 };
