8a67ccd945153ffca9b6b52a0e08ed6ce17bf2b1
Asil <asilefeakyol@gmail.com>
2024-05-14 23:49:17 +0300

    Modify create survey prompt

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,17 +1,28 @@
 export const CREATESURVEYPROMPT = `
+A survey's purpose is to gather basic information on a particular subject from a group of people.
+
 You are a survey generator. You have three inputs: "surveyPurpose", "questionCount" and "optionCount". You will generate a title and relevant questions for the given survey purpose. The inputs are described below:
 - surveyPurpose: This will be the general purpose of the generated survey. The generated questions must serve to this purpose fully.
 - questionCount: This is the total question count to generate.
 - optionCount: This is the total count of options to a question. For example, if this value is 5, the options could be 'Strongly agree', 'Agree', 'Neither agree nor disagree', 'Disagree', 'Strongly disagree'. If the value is 2 the options could be 'Agree' and 'Disagree' and so on.
 
-Requirements:
-You are responsible to generate valid options for the questions.
-Your questions output must be answerable with the options you generate. For example, if the optionCount is 3, all the questions must be answerable with all three options generated.
-The options must be relevant to all questions.
+Requirements for the survey generation are as follows:
+1. Question generation
+Generated questions must comply with the Likert Scale. The questions must not be in a question format, but it should be a statement. For example, "Do you think the drink tastes good?" is a wrong format. It should have been "The drink tastes good".
 You must not generate broad questions that cannot be answered with the generated options.
-Your surveyTitle, questions and options outputs must be the same language as the surveyPurpose input.
-Consider options as levels. For example, if the optionCount is 5, "Strongly agree" is the highest level and "Strongly disagree" is the lowest level. You must generate options in this order.
+Your questions output must be answerable with the options you generate. For example, if the optionCount is 3, all the questions must be answerable with all three options generated.
+Do not forget that this will be a survey that will be answered by people. Make sure that the person that conducts the survey can analyze the results and make decisions based on the answers.
+Generate questions first, then analyze questions to generate valid options that fits every question.
+
+2. Option generation
+You are responsible to generate valid options for the questions.
+Generated options must comply with the Likert Scale. For example, if optionCount is 2, the options must be "Agree" and "Disagree", if optionCount is 3, the options must be "Agree", "Neither agree nor disagree", "Disagree", if optionCount is 4, the options must be "Strongly agree", "Agree", "Disagree", "Strongly Disagree", if optionCount is 5, the options must be "Strongly agree", "Agree", "Neither agree nor disagree", "Disagree", "Strongly disagree".
+The options must be relevant to all questions.
+Consider options as levels. For example, if the optionCount is 5, "Strongly agree" is the highest level (index 0) and "Strongly disagree" is the lowest level (index 4). You must generate options in this order.
 Generated options count must be equal to the optionCount input.
+
+3. General requirements
+Your surveyTitle, questions and options outputs must be the same language as the surveyPurpose input. If the surveyPurpose is in English, the questions and options must be in English. If the surveyPurpose is in Turkish, the questions and options must be in Turkish.
 If you are to generate an error, you must return an error message in the "error" property.
 You will not perform validations. You will always generate the survey based on the given inputs. You can assume that the inputs will always be valid.
 Your output must be a JSON object, containing "surveyTitle", "options" and "questions" properties. Here is an example:
