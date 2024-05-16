3788c69df4ad97108fe586f57a1aa4ad51ef2b41
Vexilled <harlequinvexilled@gmail.com>
2024-05-16 22:10:35 +0300

    Ana sayfaların lokalizasyonu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import "../assets/globals.css";
 import Link from "next/link";
 import { createClient } from "@/utils/supabase/server";
 import { handleSignout } from "./actions";
+import { getLocale, getMessages, getTranslations } from "next-intl/server";
+import { NextIntlClientProvider } from "next-intl";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,13 +19,14 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const locale = await getLocale();
   const supabase = createClient();
-
+  const messages = await getMessages();
   const { data, error } = await supabase.auth.getUser();
   const isLoggedIn = !error || data?.user;
-
+  const t = await getTranslations("Navbar");
   return (
-    <html lang="en">
+    <html lang={locale}>
       <body className={inter.className}>
         <nav className="bg-gray-800 p-4">
           <div className="max-w-7xl mx-auto px-4">
@@ -33,13 +36,13 @@ export default async function RootLayout({
                   href="/predict-question-results"
                   className="text-white hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium"
                 >
-                  Predict Question Results
+                  {t("nav")}
                 </Link>
                 <Link
                   href="/create-survey"
                   className="text-white hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium"
                 >
-                  Create Survey
+                  {t("nav2")}
                 </Link>
               </div>
               <div className="flex space-x-4">
@@ -49,14 +52,14 @@ export default async function RootLayout({
                       href="/my-surveys"
                       className="text-white hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium"
                     >
-                      My Surveys
+                      {t("nav3")}
                     </Link>
                     <form>
                       <button
                         formAction={handleSignout}
                         className="text-white hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium"
                       >
-                        Log out
+                        {t("nav4")}
                       </button>
                     </form>
                   </>
@@ -65,14 +68,16 @@ export default async function RootLayout({
                     href="/login"
                     className="text-white hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium"
                   >
-                    Log in
+                    {t("nav5")}
                   </Link>
                 )}
               </div>
             </div>
           </div>
         </nav>
-        <main>{children}</main>
+        <NextIntlClientProvider messages={messages}>
+          <main>{children}</main>
+        </NextIntlClientProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,25 @@
 import Link from "next/link";
+import { useTranslations } from "next-intl";
 
 export default function Home() {
+  const t = useTranslations("Index");
   return (
     <>
       <div className="relative isolate px-6 pt-14 lg:px-8">
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="text-center">
             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-              Use the power of AI to Survey
+              {t("title")}
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-600">
-              Create surveys and predict question results in seconds.
+              {t("title2")}
             </p>
             <div className="mt-6 flex items-center justify-center gap-x-6">
               <Link
                 href="/login"
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Get started
+                {t("title3")}
               </Link>
             </div>
           </div>
