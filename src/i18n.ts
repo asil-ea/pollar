5243af017d4e130c9a6fbef4121e099fb4f66800
Asil <asilefeakyol@gmail.com>
2024-05-17 14:09:28 +0300

    Localization updates

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,15 +1,22 @@
-import {getRequestConfig} from 'next-intl/server';
+import { getRequestConfig } from "next-intl/server";
 import { cookies } from "next/headers";
- 
+
 export default getRequestConfig(async () => {
   // This can either be defined statically if only a single locale
   // is supported, or alternatively read from the user settings
   const cookieStore = cookies();
+  const cookieLocale = cookieStore.get("locale")?.value;
 
-  const locale = cookieStore.get("locale")?.value || "en";
- 
+  let locale;
+
+  if (!cookieLocale || !["en", "tr"].includes(cookieLocale)) {
+    locale = "en";
+  } else {
+    locale = cookieLocale;
+  }
+
   return {
     locale,
-    messages: (await import(`../messages/${locale}.json`)).default
+    messages: (await import(`../messages/${locale}.json`)).default,
   };
 });
