d668030932379fe50f3a0a8b601fb9a693954f6b
Asil <asilefeakyol@gmail.com>
2024-05-21 14:53:47 +0300

    Implement maintenance mode

---
+++ src/app/maintenance/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Maintenance = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-4xl font-bold">Maintenance mode, please try again later.</h1>
+    </div>
+  );
+};
+
+export default Maintenance;
