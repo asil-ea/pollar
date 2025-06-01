965555ba02a98e47bc733dd70d50e6915a01303f
Asil <asilefeakyol@gmail.com>
2025-06-01 12:33:30 +0300

    Update page.tsx

--- src/app/maintenance/page.tsx
+++ src/app/maintenance/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Maintenance = () => {
   return (
     <div className="flex justify-center items-center h-screen">
-      <h1 className="text-2xl">Thank you for your interest in this project! This app uses GPT-4o model to operate, which is not free to use. If you want a demo of this project, please contact me at asilefeakyol@gmail.com</h1>
+      <h1 className="text-2xl mx-4 text-center">Thank you for your interest in this project! This app uses GPT-4o model to operate, which is not free to use. If you want a demo of this project, please contact me at asilefeakyol@gmail.com</h1>
     </div>
   );
 };
